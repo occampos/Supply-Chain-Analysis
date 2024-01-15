eeff82f2e5295326f9a2b18bed2304da192c9a81
Olimpio Chris Campos <139114659+occampos@users.noreply.github.com>
2024-01-15 01:48:26 -0700

    Add files via upload

--- clean.sql
+++ clean.sql
@@ -16,7 +16,8 @@ FROM zz_original_data
 */
 
 --		remaining columns to be partitioned
-IF OBJECT_ID('tempdb..#partitioned_columns', 'U') IS NOT NULL DROP TABLE #partitioned_columns;
+IF OBJECT_ID('tempdb..#partitioned_columns', 'U') IS NOT NULL 
+	DROP TABLE #partitioned_columns;
 
 SELECT name
 INTO #partitioned_columns
@@ -26,10 +27,12 @@ WHERE object_id IN (
 	OBJECT_ID('products'),
 	OBJECT_ID('categories_departments'),
 	OBJECT_ID('orders'),
+	OBJECT_ID('orders_ratio'),
 	OBJECT_ID('orders_demographic'),
 	OBJECT_ID('shipping'));
 
-IF OBJECT_ID('tempdb..#remaning_columns', 'U') IS NOT NULL DROP TABLE #remaning_columns;
+IF OBJECT_ID('tempdb..#remaning_columns', 'U') IS NOT NULL 
+	DROP TABLE #remaning_columns;
 
 SELECT name
 INTO #remaning_columns
@@ -52,10 +55,6 @@ DECLARE @remaining_data NVARCHAR(MAX);
 SET @remaining_data = 'SELECT ' + @remaining_columns_list + ' FROM z_data';
 
 EXEC sp_executesql @remaining_data
-;
-
---		select data
-SELECT * FROM orders
 ;
 
 --		create customers table
@@ -114,21 +113,17 @@ IF OBJECT_ID('dbo.orders', 'U') IS NOT NULL
 
 SELECT DISTINCT
 	Order_Id,
-	Order_Item_Cardprod_Id,
+	Order_Item_Id,
+	Order_Customer_Id,
 	order_date_DateOrders,
 	Type,
-	Order_Customer_Id,
-	Order_Item_Id,
+	Order_Item_Cardprod_Id,
 	Order_Item_Product_Price,
 	Order_Item_Quantity,
+	Sales,
 	Order_Item_Discount_Rate,
 	Order_Item_Discount,
-	Sales,
-	Order_Item_Total,
-	Order_Item_Profit_Ratio,
-	Order_Profit_Per_Order,
-	Benefit_per_order,
-	Sales_per_customer
+	Order_Item_Total
 INTO orders 
 FROM  z_data
 ;
@@ -150,15 +145,33 @@ GROUP BY Order_Item_Id
 ORDER BY n_occurences DESC
 ;
 
+--		create orders_ratio table
+IF OBJECT_ID('dbo.orders_ratio', 'U') IS NOT NULL
+    DROP TABLE dbo.orders_ratio;
+
+SELECT DISTINCT
+	Order_Id,
+	Order_Item_Id,
+	Order_Customer_Id,
+	order_date_DateOrders,
+	Order_Item_Total,
+	Order_Item_Profit_Ratio,
+	Order_Profit_Per_Order,
+	Benefit_per_order,
+	Sales_per_customer
+INTO orders_ratio 
+FROM  z_data
+;
+
 --		create orders_demographic table
 IF OBJECT_ID('dbo.orders_demographic', 'U') IS NOT NULL
     DROP TABLE dbo.orders_demographic;
 
-SELECT 
+SELECT DISTINCT
 	Order_Id,
+	Order_Item_Id,
 	order_date_DateOrders,
 	Type,
-	Order_Item_Id,
 	Order_Customer_Id,
 	Latitude,
 	Longitude,
@@ -199,7 +212,8 @@ FROM z_data
 		x customers
 		x products
 		x categories_departments
-		orders
+		x orders
+		x orders_ratio
 		orders_demographic
 		shipping
 */
@@ -207,10 +221,6 @@ FROM z_data
 /* 
 	clean customers table
 */
-
---		select
-SELECT * FROM customers
-;
 
 --		table information
 SELECT 
@@ -241,8 +251,7 @@ EXEC sp_rename 'customers.Customer_Password', 'password', 'COLUMN'
 ;
 
 --		check for error (?) in characters, there are none
-DECLARE @char_error CHAR(1) = '?'
-;
+DECLARE @char_error CHAR(1) = '?';
 
 SELECT *
 FROM customers
@@ -411,13 +420,13 @@ DROP COLUMN
 	password
 ;
 
+--		select
+SELECT * FROM customers
+;
+
 /* 
 	clean products table
 */
-
---		select
-SELECT * FROM products
-;
 
 --		table information
 SELECT 
@@ -556,14 +565,21 @@ ALTER TABLE products
 ALTER COLUMN price DECIMAL(10, 2)
 ;
 
+SELECT
+	column_name, 
+	data_type, 
+	character_maximum_length
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'products'
+;
+
+--		select
+SELECT * FROM products
+;
+
 /* 
 	clean categories_departments table
 */
-
---		select
-SELECT * 
-FROM categories_departments
-;
 
 --		table information
 SELECT 
@@ -648,13 +664,14 @@ WHERE
 	department IS NULL 
 ;
 
+--		select
+SELECT * 
+FROM categories_departments
+;
+
 /* 
 	clean orders table
 */
-
---		select
-SELECT * FROM orders
-;
 
 --		table information
 SELECT 
@@ -665,31 +682,197 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'orders'
 ;
 
-xxxxxxxxxxxxxxxxxxxx
-
 --		rename column names
 SELECT name
 FROM sys.columns 
 WHERE object_id = OBJECT_ID('orders')
 ;
 
-EXEC sp_rename 'customers.Order_Id', 'customer_id', 'COLUMN';
+EXEC sp_rename 'orders.Order_Id', 'order_id', 'COLUMN'
+EXEC sp_rename 'orders.Order_Item_Cardprod_Id', 'product_id', 'COLUMN'
+EXEC sp_rename 'orders.order_date_DateOrders', 'order_date', 'COLUMN'
+EXEC sp_rename 'orders.Type', 'payment_type', 'COLUMN'
+EXEC sp_rename 'orders.Order_Customer_Id', 'customer_id', 'COLUMN'
+EXEC sp_rename 'orders.Order_Item_Id', 'transaction_id', 'COLUMN'
+EXEC sp_rename 'orders.Order_Item_Product_Price', 'price', 'COLUMN'
+EXEC sp_rename 'orders.Order_Item_Quantity', 'quantity', 'COLUMN'
+EXEC sp_rename 'orders.Order_Item_Discount_Rate', 'discount_pct', 'COLUMN'
+EXEC sp_rename 'orders.Order_Item_Discount', 'discount', 'COLUMN'
+EXEC sp_rename 'orders.Sales', 'gross_sale', 'COLUMN'
+EXEC sp_rename 'orders.Order_Item_Total', 'net_sale', 'COLUMN'
+;
+
+--		change order_date datatypes to datetime
+ALTER TABLE orders
+ALTER COLUMN order_date DATETIME
+;
+
+--		change datatypes to decimal
+ALTER TABLE orders
+ALTER COLUMN price DECIMAL(10,2);
+ALTER TABLE orders
+ALTER COLUMN gross_sale DECIMAL(10,2);
+ALTER TABLE orders
+ALTER COLUMN discount DECIMAL(10,2);
+ALTER TABLE orders
+ALTER COLUMN net_sale DECIMAL(10,2);
 ;
 
 
+--		check prices against products
+IF OBJECT_ID('tempdb..#check_price', 'U') IS NOT NULL 
+	DROP TABLE #check_price;
 
+SELECT *
+INTO #check_price
+FROM 
+	(SELECT
+		product_id AS o_product_id,
+		price AS o_price
+	FROM orders) o LEFT OUTER join products p ON o.o_product_id=p.product_id
+;
 
+--			check nulls, no nulls, all products in orders table are present in products table
+SELECT *
+FROM #check_price
+WHERE 
+	o_product_id IS NULL OR
+	o_price IS NULL OR
+	category_id IS NULL OR
+	product_id IS NULL
+;
 
+--			price match, all prices in orders table match with prices in products table
+SELECT DISTINCT
+	o_product_id,
+	o_price,
+	product_id,
+	price,
+	o_price - price AS diff
+FROM 
+	(SELECT
+		product_id AS o_product_id,
+		price AS o_price
+	FROM orders) o join products p ON o.o_product_id=p.product_id
+WHERE o_price - price > 0
+; 
 
+--		check if changing columsn to decimals(10,2) created accounting error
+IF OBJECT_ID('tempdb..#decimal_check', 'U') IS NOT NULL 
+	DROP TABLE #decimal_check;
 
+SELECT
+	transaction_id,
+	price,
+	quantity,
+	discount_pct,
+	CAST((price * quantity) * discount_pct AS DECIMAL(10,2)) AS  test,
+	discount
+INTO #decimal_check
+FROM orders
+;
 
+--			discount_pct and resulting dicount are wrong for all rows with discount_pct = 0.059999999 
+SELECT *
+FROM #decimal_check
+WHERE test != discount
+;
 
+SELECT COUNT(*)
+FROM #decimal_check
+WHERE test != discount;
+;
 
+SELECT COUNT(*)
+FROM orders
+WHERE discount_pct = 0.059999999
+;
 
+--			fix error by updating discount_pct to 0.055013753 
+SELECT transaction_id
+FROM orders
+WHERE discount_pct = 0.059999999
+;
 
+UPDATE orders
+SET discount_pct = 0.055013753
+WHERE transaction_id IN (SELECT transaction_id
+						FROM orders
+						WHERE discount_pct = 0.059999999)
+;
 
+--		check if changing columsn to decimals(10,2) created accounting error
+IF OBJECT_ID('tempdb..#decimal_check', 'U') IS NOT NULL 
+	DROP TABLE #decimal_check;
 
+SELECT
+	transaction_id,
+	price,
+	quantity,
+	discount,
+	CAST((price * quantity) AS DECIMAL(10,2)) AS  test,
+	gross_sale
+INTO #decimal_check
+FROM orders
+;
 
+SELECT *
+FROM #decimal_check
+WHERE test != gross_sale
+;
+
+IF OBJECT_ID('tempdb..#decimal_check', 'U') IS NOT NULL 
+	DROP TABLE #decimal_check;
+
+SELECT
+	transaction_id,
+	price,
+	quantity,
+	discount,
+	gross_sale,
+	CAST(gross_sale - discount AS DECIMAL(10,2)) AS  test,
+	net_sale
+INTO #decimal_check
+FROM orders
+;
+
+--		some net_sales are off by 0.01
+SELECT *
+FROM #decimal_check
+WHERE test != net_sale
+;
+
+SELECT transaction_id
+FROM #decimal_check
+WHERE test != net_sale
+;
+
+WITH cte AS (
+	SELECT
+		*,
+		net_sale - test AS diff
+	FROM #decimal_check
+	WHERE test != net_sale)
+SELECT *
+FROM cte
+WHERE diff != 0.01
+;
+
+--		fix error by subtracting 0.01 to corresponding net sales
+UPDATE orders
+SET net_sale = net_sale - 0.01
+WHERE transaction_id IN (SELECT transaction_id
+						FROM #decimal_check
+						WHERE test != net_sale)
+;
+
+--	select
+SELECT * FROM orders
+;
+
+/* 
+	clean orders_ratio table
+*/
 
 --		table information
 SELECT 
@@ -697,7 +880,113 @@ SELECT
 	data_type, 
 	character_maximum_length
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'orders_demographic';
+WHERE TABLE_NAME = 'orders_ratio'
+;
+
+--		rename column names
+SELECT name
+FROM sys.columns 
+WHERE object_id = OBJECT_ID('orders_ratio')
+;
+
+EXEC sp_rename 'orders_ratio.Order_Id', 'order_id', 'COLUMN';
+EXEC sp_rename 'orders_ratio.Order_Item_Id', 'transaction_id', 'COLUMN';
+EXEC sp_rename 'orders_ratio.Order_Customer_Id', 'customer_id', 'COLUMN';
+EXEC sp_rename 'orders_ratio.order_date_DateOrders', 'order_date', 'COLUMN';
+EXEC sp_rename 'orders_ratio.Order_Item_Total', 'net_sale', 'COLUMN';
+EXEC sp_rename 'orders_ratio.Order_Item_Profit_Ratio', 'item_profit_ratio', 'COLUMN';
+EXEC sp_rename 'orders_ratio.Order_Profit_Per_Order', 'profit_per_order', 'COLUMN';
+EXEC sp_rename 'orders_ratio.Benefit_per_order', 'benefit_per_order', 'COLUMN';
+EXEC sp_rename 'orders_ratio.Sales_per_customer', 'sales_per_customer', 'COLUMN';
+;
+
+--		change order_date datatypes to datetime
+ALTER TABLE orders_ratio
+ALTER COLUMN order_date DATETIME
+;
+
+--		change datatypes to decimal
+ALTER TABLE orders_ratio
+ALTER COLUMN net_sale DECIMAL(10,2);
+;
+
+--		some net_sales are off by 0.01
+--		fix error by subtracting 0.01 to corresponding net sales
+WITH cte AS (
+	SELECT
+		o.transaction_id,
+		o.net_sale as o,
+		r.net_sale as r
+	FROM orders o join orders_ratio r on o.transaction_id = r.transaction_id)
+SELECT *
+FROM cte
+WHERE o != r
+
+WITH cte AS (
+	SELECT
+		o.transaction_id,
+		o.net_sale as o,
+		r.net_sale as r
+	FROM orders o join orders_ratio r on o.transaction_id = r.transaction_id)
+SELECT transaction_id
+INTO #error_transaction_id
+FROM cte
+WHERE o != r
+
+UPDATE orders_ratio
+SET net_sale = net_sale - 0.01
+WHERE transaction_id IN (SELECT transaction_id
+						FROM #error_transaction_id)
+;
+
+--	select
+SELECT * FROM orders_ratio
+;
+
+/* 
+	clean orders_demographic table
+*/
+
+--		table information
+SELECT 
+	column_name, 
+	data_type, 
+	character_maximum_length
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'orders_demographic'
+;
+
+--		rename column names
+SELECT name
+FROM sys.columns 
+WHERE object_id = OBJECT_ID('orders_demographic')
+;
+
+EXEC sp_rename 'orders_demographic.Order_Id', 'order_id', 'COLUMN';
+EXEC sp_rename 'orders_demographic.order_date_DateOrders', 'order_date', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Type', 'type', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_Item_Id', 'transaction_id', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_Customer_Id', 'customer_id', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Latitude', 'latitiude', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Longitude', 'longitude', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Market', 'market', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_Region', 'region', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_Country', 'country', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_State', 'state', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_City', 'city', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_Zipcode', 'zipcode', 'COLUMN';
+EXEC sp_rename 'orders_demographic.Order_Status', 'order_status', 'COLUMN';
+;
+
+
+--	select
+SELECT * FROM orders_demographic
+;
+
+
+
+
+
 
 
 
@@ -717,12 +1006,12 @@ WHERE EXISTS (
         AND CHARACTER_MAXIMUM_LENGTH >= LEN(@char_error)
         AND (
             CHARINDEX(@char_error, Type) > 0 OR
-            CHARINDEX(@char_error, Market) > 0 OR
-			CHARINDEX(@char_error, Order_Region) > 0 OR
-			CHARINDEX(@char_error, Order_Country) > 0 OR
-			CHARINDEX(@char_error, Order_State) > 0 OR
-			CHARINDEX(@char_error, Order_City) > 0 OR
-			CHARINDEX(@char_error, Order_Zipcode) > 0 OR
-			CHARINDEX(@char_error, Order_Status) > 0 
+            CHARINDEX(@char_error, market) > 0 OR
+			CHARINDEX(@char_error, region) > 0 OR
+			CHARINDEX(@char_error, country) > 0 OR
+			CHARINDEX(@char_error, state) > 0 OR
+			CHARINDEX(@char_error, city) > 0 OR
+			CHARINDEX(@char_error, zipcode) > 0 OR
+			CHARINDEX(@char_error, order_status) > 0 
         )
 );
